b722a9a975cc11cc188e48593320147c9b3b3998
Gregory Smith <gregjsmith@gmx.com>
2022-03-18 22:16:35 -0400

    update configuration.nix for flakes

--- configuration.nix
+++ configuration.nix
@@ -12,12 +12,12 @@
     ];
 
   #nix flakes (https://nixos.wiki/wiki/flakes#Installing_flakes)
-  #nix = {
-  #	package = pkgs.nixUnstable; #or versioned attributes like nix_2_4
-  #	extraOptions = ''
-  #		experimental-features = nix-command flakes
-  #	'';
-  #};
+  nix = {
+  	package = pkgs.nixUnstable; #or versioned attributes like nix_2_4
+  	extraOptions = ''
+  		experimental-features = nix-command flakes
+  	'';
+  };
   
 
   #KERNEL/BOOTUP#
