60a3f303e3e2437f6243e07dfdfa3d49597a0077
Gregory Smith <gregjsmith@gmx.com>
2022-03-20 12:49:46 -0400

    update project structure

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./system/hardware-configuration.nix
+      ./modules/system/hardware-configuration.nix
       ./modules/secrets/network.nix
     ];
 
