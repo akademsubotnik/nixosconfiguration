9b436692d5ee26609130b389f9ddcac89f817301
Greg <44036625+gregg00@users.noreply.github.com>
2023-02-01 19:55:45 +0200

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,28 +18,57 @@
 
   targets.genericLinux.enable = true;
 
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.bat.enable = true;
 
   programs.git = {
     enable = true;
-    userName  = "gregg00";
+    userName = "gregg00";
     userEmail = "gregjsmith@gmx.com";
-  };
+  }; 
+
 
 
   programs.bash = {
     enable = true;
     bashrcExtra = ''
-      . ~/.bashrc-old
+      export EDITOR="vi";
+      PATH="/home/greg/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/Users/Chuwi/AppData/Local/Microsoft/WindowsApps:/snap/bin" ;
+
+      # enable color support of ls and also add handy aliases
+      if [ -x /usr/bin/dircolors ]; then
+        test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+        alias ls='ls --color=auto'
+        
+        alias grep='grep --color=auto'
+        alias fgrep='fgrep --color=auto'
+        alias egrep='egrep --color=auto'
+      fi 
+
+      # some more ls aliases
+      alias ll='ls -alF'
+      alias la='ls -A'
+      alias l='ls -CF'
+
+      # append to the history file, don't overwrite it
+      shopt -s histappend
+
+      ## Enables displaying colors in the terminal
+      force_color_prompt=yes
+
+      #Have terminal name have color
+      PS1='\e[32;1m\u@\h: \e[32m\W\e[32m\$ '
+
+
+      #Packages you want to install
+      home.packages = [
+        pkgs.tmux
+      ];
+
+
+
     '';
   };
-
-
-  home.packages = [
-    pkgs.tmux
-  ];
-
 }
